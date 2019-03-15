8c2c1c36f8d229059118ac4c65f9717b2115b5d7
Mauricio García <mainsaw_12@live.com.mx>
2019-03-14 23:07:36 -0600

    Primer Programa C#

---
+++ Console/RecorridoMatriz/RecorridoMatriz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecorridoMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[4,4] { {1,3,5,7},
+                                         {4,7,9,7},
+                                         {2,6,5,2},
+                                         {2,4,5,2}};
+            int num=3;
+
+            for(int y = 0; y <array.Length; y++)
+            {
+                for(int x = 0; x <array.Length; x++)
+                {
+                    if (y % 2 == 0)
+                    {
+                        num = array[x, y];
+                        Console.WriteLine(num);
+                    }
+                    else
+                    {
+                        num = array[num- x, y];
+                        Console.WriteLine(num);
+                    }
+                }
+            }
+
+        }
+    }
+}

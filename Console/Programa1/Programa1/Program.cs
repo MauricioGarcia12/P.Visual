e178605c0df16032ba284a00642acd0163a0f4cc
Mauricio García <mainsaw_12@live.com.mx>
2019-03-20 19:22:49 -0600

    Programa1

---
+++ Console/Programa1/Programa1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Tamaño de Bytes de en Short: " + sizeof(short));
+            Console.WriteLine();
+
+            Console.WriteLine("Tamaño de Bytes de en Int: " + sizeof(int));
+            Console.WriteLine();
+
+            Console.WriteLine("Tamaño de Bytes de en Long: " + sizeof(long));
+            Console.WriteLine();
+
+            Console.WriteLine("Tamaño de Bytes de en float: " + sizeof(float));
+            Console.WriteLine();
+
+            Console.WriteLine("Tamaño de Bytes de en double: " + sizeof(double));
+            Console.WriteLine();
+
+            Console.WriteLine("Tamaño de Bytes de en decimal: " + sizeof(decimal));
+            Console.WriteLine();
+
+            Console.WriteLine("Tamaño de Bytes de en Char: " + sizeof(char));
+            Console.WriteLine();
+
+            Console.WriteLine("Tamaño de Bytes de en boolean: " + sizeof(Boolean));
+            Console.WriteLine();
+
+            Console.WriteLine("Short mínimo: " + short.MinValue);
+            Console.WriteLine("Short máximo: " + short.MaxValue);
+
+            Console.WriteLine("Int mínimo: "+int.MinValue);
+            Console.WriteLine("Int máximo: " + int.MaxValue);
+
+            Console.WriteLine("long mínimo: " + long.MinValue);
+            Console.WriteLine("long máximo: " + long.MaxValue);
+
+            Console.WriteLine("float mínimo: " + float.MinValue);
+            Console.WriteLine("float máximo: " + float.MaxValue);
+
+            Console.WriteLine("double mínimo: " + double.MinValue);
+            Console.WriteLine("double máximo: " + double.MaxValue);
+
+            Console.WriteLine("boolean mínimo:0" );
+            Console.WriteLine("boolean máximo:1" );
+
+           
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

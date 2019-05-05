6e18cf7df1c70cb82e6a0742e2282f4d18925472
Mauricio García <mainsaw_12@live.com.mx>
2019-05-04 23:11:55 -0500

    Delegar

---
+++ Console/PreExamen/PreExamen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace PreExamen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numeros = new int[5, 5];
+            //GetLength sí es cero es para filas 1 para columnas
+            for(int y = 0; y < numeros.GetLength(0); y++)
+            {
+                for(int x = 0; x < numeros.GetLength(1); x++)
+                {
+                    MessageBox.Show
+                }
+            }
+
+        }
+    }
+}

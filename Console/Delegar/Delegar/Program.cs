06472ba8fa1dc68c69589f586e5caaeecf0f0b3f
Mauricio García <mainsaw_12@live.com.mx>
2019-05-04 23:10:21 -0500

    Programa Delegar

---
+++ Console/Delegar/Delegar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegar
+{
+    public delegate void delegado(String cadena);
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            delegado d1 = new delegado(MessageOne.message1);
+
+            d1("Este es el mensaje 1");
+
+            d1 = new delegado(MessageTwo.message2);
+            d1("Este es el mensaje 2");
+
+        }
+    }
+
+    class MessageOne
+    {
+        public static void message1(String mensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine( mensaje);
+        }
+    }
+
+    class MessageTwo
+    {
+        public static void message2(String mensaje)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine( mensaje);
+        }
+    }
+}

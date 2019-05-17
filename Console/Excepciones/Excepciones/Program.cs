4800896d449015582098249eaf8bdab9ea2f7000
Mauricio García <mainsaw_12@live.com.mx>
2019-05-17 10:14:05 -0500

    Control de Excepciones

---
+++ Console/Excepciones/Excepciones/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    class Program :System.Exception
+    {
+        public static List<String> miLista;
+        static void Main(string[] args)
+        {
+            
+               //miLista = new List<string>();
+           
+
+            try
+            {
+                miLista.Add("Objeto");
+                metodoExcepcion();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No se creo el objeto");
+            }
+            Console.ReadLine();
+
+        }
+        private static void metodoExcepcion()
+        {
+            throw new Exception("Algo salío mal");
+        }
+    }
+}

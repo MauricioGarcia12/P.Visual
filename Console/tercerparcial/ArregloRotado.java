89881fc4b7c76d42fd8680e2d20a25315e196305
Mauricio García <mainsaw_12@live.com.mx>
2019-06-21 16:32:40 -0500

    Rotado nuevo

--- Console/tercerparcial/ArregloRotado.java
+++ Console/tercerparcial/ArregloRotado.java
@@ -1,45 +1,32 @@
-
 package tercerparcial;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import javax.swing.JOptionPane;
 
-
 public class ArregloRotado {
 
-   
     public static void main(String[] args) {
 
-       int tamañoArreglo=Integer.parseInt(JOptionPane.showInputDialog("Digite el tamaño del arreglo"));
-       int numeroRotaciones=Integer.parseInt(JOptionPane.showInputDialog("Digite el número de rotaciones"));
-       int[] numeros= new int[tamañoArreglo];
-       
-       for(int i=0;i<tamañoArreglo;i++){
-           numeros[i]=Integer.parseInt(JOptionPane.showInputDialog("Digite los valores"));
-       }
-       JOptionPane.showMessageDialog(null,"Su arreglo es");
-       for(int i=0;i<tamañoArreglo;i++){
-        JOptionPane.showMessageDialog(null,Arrays.toString(numeros));
-       }
-       
-       
-        int temp = numeros[0];
-        for(int i=0;i<numeros.length;i++){
-            numeros[i] = numeros[i+1];
-        }
-        numeros[numeros.length-1] = temp;
-       
-        for(int i=0;i<numeros.length;i++){
-           JOptionPane.showMessageDialog(null, numeros);
-        }
-       
+        int tamañoArreglo = Integer.parseInt(JOptionPane.showInputDialog("Digite el tamaño del arreglo"));
+        int numeroRotaciones = Integer.parseInt(JOptionPane.showInputDialog("Digite el número de rotaciones"));
+        double temp;
+        ArrayList<Double> numeros = new ArrayList<Double>(tamañoArreglo);
 
-        
-        
-        
-        
-        
-        
+        for (double i = 0; i < tamañoArreglo; i++) {
+            numeros.add(Double.parseDouble(JOptionPane.showInputDialog("Digite un valor:")));
+        }
+        JOptionPane.showMessageDialog(null, "Lista: " + numeros.toString());
+
+//        Orden hacia la izquierda
+        for (int i = 0; i < numeroRotaciones; i++) {
+            temp = numeros.get(0);//Obtiene primer dato de la lista siempre
+            numeros.remove(0); //remueve ese dato de la lista
+            numeros.add(temp);//ingresa 1 al final del arraylist lo coloca al final de la lista
+
+        }
+        JOptionPane.showMessageDialog(null, "Lista rotada " + numeros.toString());
+
     }
-    
+
 }
